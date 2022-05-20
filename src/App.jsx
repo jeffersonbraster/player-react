3f05082ab98102c69a97a283b0f90004fa3d3b58
Jefferson <jeferson.oliveira@g.globo>
2022-05-19 23:28:30 -0300

    usando mock de musicas e estilizando

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
+
+import data from "./util";
 
 import "./styles/app.scss";
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[1]);
+
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
