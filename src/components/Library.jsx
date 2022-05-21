aeaff3262d8b864f77d4c12746a0bbe107356d28
Jefferson <jeferson.oliveira@g.globo>
2022-05-20 21:28:21 -0300

    selecionando song da biblioteca

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -1,12 +1,18 @@
 import LibrarySong from "./LibrarySong";
 
-const Library = ({ songs }) => {
+const Library = ({ songs, setCurrentSong }) => {
   return (
     <div className="library">
       <h2>Library</h2>
       <div className="library-songs">
         {songs.map((song) => (
-          <LibrarySong song={song} />
+          <LibrarySong
+            key={song.id}
+            setCurrentSong={setCurrentSong}
+            songs={songs}
+            song={song}
+            id={song.id}
+          />
         ))}
       </div>
     </div>
--- src/components/LibrarySong.jsx
+++ src/components/LibrarySong.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-const LibrarySong = ({ song }) => {
+const LibrarySong = ({ song, songs, setCurrentSong, id }) => {
+  const songSelectedHandler = async () => {
+    await setCurrentSong(song);
+  };
   return (
-    <div className="library-song">
+    <div onClick={songSelectedHandler} className="library-song">
       <img src={song.cover} alt={song.name} />
 
       <div className="song-description">
